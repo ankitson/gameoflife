c118e22cb6f1a4031fe239ae8ed9b45d8f67828f
Ankit Soni <ankitsoni9@gmail.com>
2016-08-30 21:18:57 -0700

    Much better perf and grid drawing
    Handles 100x100 at 5ms/frame. can go upto 200-300
    Draw grid only once and layer on top

--- js/src/main/scala/life/GameOfLifeJS.scala
+++ js/src/main/scala/life/GameOfLifeJS.scala
@@ -6,19 +6,25 @@ import scala.scalajs.js
 import scala.scalajs.js.Any._
 import js.annotation.JSExport
 import org.scalajs.dom
-import org.scalajs.dom.KeyboardEvent
+import org.scalajs.dom.{FocusEvent, KeyboardEvent}
 import org.scalajs.dom.ext.{Color, KeyCode}
-import org.scalajs.dom.raw.{HTMLCanvasElement, HTMLInputElement}
+import org.scalajs.dom.raw.{HTMLCanvasElement, HTMLDivElement, HTMLInputElement}
+
 
 @JSExport
 object GameOfLifeJS extends js.JSApp {
-
   var run = true
-  var board = Board.randomBoard(10,10,0.1)
-  val canvas = dom.document.getElementById("life").asInstanceOf[dom.html.Canvas]
+  var board = Board.randomBoard(100,100,0.1)
+
+  val canvas = dom.document.getElementById("life-board").asInstanceOf[dom.html.Canvas]
   canvas.width = canvas.offsetWidth.toInt
   canvas.height = canvas.offsetHeight.toInt
   val ctx = canvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
+
+  val gridCanvas = dom.document.getElementById("life-grid").asInstanceOf[dom.html.Canvas]
+  gridCanvas.width = gridCanvas.offsetWidth.toInt
+  gridCanvas.height = gridCanvas.offsetHeight.toInt
+  val gridCtx = gridCanvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
 
   var cellWidth = canvas.width/board.m
   var cellHeight = canvas.height/board.n
@@ -26,11 +32,30 @@ object GameOfLifeJS extends js.JSApp {
   def init() = {
     cellWidth = canvas.width/board.m
     cellHeight= canvas.height/board.n
-    ctx.strokeStyle = Color.Black.toString()
-    ctx.lineWidth = 1
-    ctx.moveTo(0,0)
 
+    gridCtx.fillStyle = Color.White.toString()
+    gridCtx.clearRect(0,0,canvas.width,canvas.height)
+
+    gridCtx.strokeStyle = Color.Black.toString()
+    gridCtx.lineWidth = 1
+    for (startX <- 0 to canvas.width by cellWidth) {
+      gridCtx.beginPath()
+      gridCtx.moveTo(startX,0)
+      gridCtx.lineTo(startX,canvas.height)
+      gridCtx.closePath()
+      gridCtx.stroke()
+    }
+    for (startY <- 0 to canvas.height by cellHeight) {
+      gridCtx.beginPath()
+      gridCtx.moveTo(0,startY)
+      gridCtx.lineTo(canvas.width,startY)
+      gridCtx.closePath()
+      gridCtx.stroke()
+    }
+
+    ctx.fillStyle = Color.White.toString()
     ctx.clearRect(0,0,canvas.width,canvas.height)
+    ctx.fillRect(0,0,canvas.width,canvas.height)
   }
 
 
@@ -38,9 +63,23 @@ object GameOfLifeJS extends js.JSApp {
   stats.showPanel( 1 ); // 0: fps, 1: ms, 2: mb, 3+: custom
   dom.document.body.appendChild( stats.dom.asInstanceOf[org.scalajs.dom.raw.Node] )
 
-  def draw(): Unit = {
+  var lastCall = 0d
+  val fpsDisplay = dom.document.getElementById("fps")
+
+  var animFrameHandle = 0
+
+  def draw(timestamp:Double): Unit = {
+    println("draw loop running")
+    if (lastCall == 0) {
+      lastCall = timestamp
+    } else {
+      val duration = timestamp - lastCall
+      //println(f"setting duration to ${timestamp - lastCall}")
+      fpsDisplay.innerHTML = duration.toString + "ms/frame"
+      lastCall = timestamp
+    }
     stats.begin()
-    val (boardArr,boardArr2) = board.currentAndPrev()
+    val (boardArr, boardArr2) = board.currentAndPrev()
     for (row <- 0 until board.m) {
       for (col <- 0 until board.n) {
         val xcoord = col * cellWidth
@@ -55,48 +94,70 @@ object GameOfLifeJS extends js.JSApp {
           val startY = ycoord
           val width = cellWidth
           val height = cellHeight
-          ctx.fillRect(startX,startY,width,height)
+          ctx.fillRect(startX, startY, width, height)
         }
       }
     }
+
     board.step()
     stats.end()
 
-    if (run) dom.window.requestAnimationFrame((_:Double) => draw())
+    if (run) animFrameHandle = dom.window.requestAnimationFrame(draw _)
   }
 
   def main(): Unit = {
     println("hello from scalajs")
 
-    dom.document.onclick = {(evt:dom.MouseEvent) =>
-        run = !run
-        if (run) {
-          init()
-          dom.window.requestAnimationFrame((_:Double) => draw())
-        }
+    //var drawerHandle = 0
+
+    //TODO: is setInterval faster than reqAnimFrame? (shouldnt be)
+
+    //TODO: grid layout bug
+
+    //TODO: Adding this reduces perf dramatically and screws up the fps monitor...??idonteven
+//    dom.document.getElementById("life").asInstanceOf[HTMLDivElement].onkeypress = (event:KeyboardEvent) => {
+//      if (event.keyCode == KeyCode.Right) draw(dom.window.performance.now())
+//      if (event.keyCode == KeyCode.Space) toggleRun()
+//      if (event.keyCode == KeyCode.N) board = Board.randomBoard(board.n,board.m,0.1); startRun()
+//    }
+
+
+    def toggleRun() = { if (run) stopRun() else animFrameHandle = startRun() }
+
+    def startRun() = {
+      run = true
+      init()
+      //drawerHandle = dom.window.setInterval(draw _, 0)
+      dom.window.requestAnimationFrame(draw _)
     }
 
-
-    dom.document.onkeydown = (event:KeyboardEvent) => {
-      if (event.keyCode == KeyCode.Right) draw()
-      if (event.keyCode == KeyCode.Space) run = !run; if (run) { init(); dom.window.requestAnimationFrame((_:Double) => draw()) }
-    }
-
-    dom.document.getElementById("size").asInstanceOf[HTMLInputElement].onkeypress = {(evt:KeyboardEvent) =>
+    def stopRun() = {
+      //dom.window.clearInterval(drawerHandle)
       run = false
-      try {
-        val newSize = dom.document.getElementById("size").asInstanceOf[HTMLInputElement].value.toInt
-        dom.document.getElementById("sizeShow").innerHTML = newSize.toString
-        board = Board.randomBoard(newSize, newSize)
-        run = true
-        init()
-        dom.window.requestAnimationFrame((_: Double) => draw())
-      } catch {
+      dom.window.cancelAnimationFrame(animFrameHandle)
+    }
+
+    dom.document.getElementById("size").asInstanceOf[HTMLInputElement].onfocus = {(_:FocusEvent) =>
+      dom.document.getElementById("size").asInstanceOf[HTMLInputElement].value =  ""
+    }
+    dom.document.getElementById("size").asInstanceOf[HTMLInputElement].onkeypress = {(evt:KeyboardEvent) =>
+      evt.keyCode match {
+        case KeyCode.Enter =>
+          stopRun()
+          try {
+            val newSize = dom.document.getElementById("size").asInstanceOf[HTMLInputElement].value.toInt
+            dom.document.getElementById("sizeShow").innerHTML = newSize.toString
+            board = Board.randomBoard(newSize, newSize)
+            startRun()
+          } catch {
+            case _ => ()
+          }
         case _ => ()
       }
+
     }
 
-    init()
-    dom.window.requestAnimationFrame((_:Double) => draw())
+    //dom.window.setInterval(draw _,1000/60)
+    startRun()
   }
 }
